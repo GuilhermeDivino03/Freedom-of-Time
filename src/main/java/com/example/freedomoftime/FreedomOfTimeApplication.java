5467a17a54d07c3d6a6047360dec147e1ee0353f
GuilhermeDivino03 <guilhermedivino2003@gmail.com>
2025-07-23 11:59:23 -0300

    Estrura do projeto

---
+++ src/main/java/com/example/freedomoftime/FreedomOfTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.freedomoftime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreedomOfTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreedomOfTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freedomoftime/FreedomOfTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freedomoftime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreedomOfTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
